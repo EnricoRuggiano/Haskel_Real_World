ec729a9f2d8bb7af4b4fbf52805fdb31ce72439a
ubuntu <enricoruggiano1@gmail.com>
2023-07-22 20:21:17 +0200

    [add] 03

--- ch03/ListADT.hs
+++ ch03/ListADT.hs
@@ -7,3 +7,15 @@ fromList [] = Nil
 
 toList Nil = []
 toList (Cons x xs) = x: toList xs
+
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+myMean t = if null t
+        then 0
+        else
+            fromIntegral(mySum(t)) / fromIntegral(myLength(t))
+
